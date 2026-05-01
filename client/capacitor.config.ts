cdfcfc9807f05ab34f662632dc42abdeecc5a389
Pydimarri Neha Santhoshini <nehapydimarri@gmail.com>
2026-05-01 16:15:53 +0530

    Updated website title and added Capacitor config

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.prave.healthvault',
+  appName: 'Health Vault',
+  webDir: 'build'
+};
+
+export default config;
